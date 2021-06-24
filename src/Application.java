3cd4cbd9ea2bc60ff0edb4a6639e2b58808fbfaa
Администратор <a161rnd@gmail.com>
2021-06-24 16:11:46 +0300

    commit1

---
+++ src/Application.java
@@ -0,0 +1,16 @@
+public class Application {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        //int x = 0;
+        a = count(a);
+        System.out.println(a);
+    }
+
+    public static int count(int x) {
+        x = x + 2;
+        return x;
+    }
+
+    }
+
---
+++ src/Test1.java
@@ -0,0 +1,32 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("dgdfg");
+
+        int a = 2;
+        switch (a) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                System.out.println("4");
+                break;
+            default:
+                System.out.println("def");
+                break;
+        }
+        int val = switch (a) {
+            case 1 -> 91;
+            case 2 -> 92;
+            case 3 -> 93;
+            case 4 -> 94;
+            default -> 123;
+        };
+        System.out.println(val);
+    }
+}
