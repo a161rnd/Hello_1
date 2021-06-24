7980c36c2d81ba12db3a50fdb1b0558e9befeb9c
Администратор <a161rnd@gmail.com>
2021-06-24 16:20:52 +0300

    commit1

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("dgdfg");
-
+        int b = 55;
         int a = 2;
         switch (a) {
             case 1:
